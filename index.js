7bdfeaf18b6914bb1a1fc547310639909c3dd113
Uryn Dmytro <44302526+tedi4t@users.noreply.github.com>
2025-03-12 23:36:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
+
+
 // Create 'uploads' folder if it doesn't exist
 const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
